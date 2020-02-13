7b4a7c2e99cf2aa34624fd5ec158f52cfaf867e4
Jackyjk <2538808265@qq.com>
2020-02-13 10:35:00 +0800

    header

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="login_container">
-      <div class="login_box"></div>
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,14 +18,32 @@ export default {}
   height: 100%;
 }
 
-.login_box{
-    width: 450px;
-    height: 300px;
-    background-color: #ffffff;
-    border-radius: 3px;
-    position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left:50%;
+  transform: translate(-50%,-50%);
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
 }
 </style>
