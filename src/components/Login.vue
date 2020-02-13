da7f2a0c64e3720239dfe68d9dbdc2c6a66deeb3
Jackyjk <2538808265@qq.com>
2020-02-13 12:28:39 +0800

    表单自动验证  提交数据测试  完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,29 @@
         <img src="../assets/logo.png" alt />
       </div>
       <!-- login form -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">Login</el-button>
-          <el-button type="info">Reset</el-button>
+          <el-button type="primary" @click="login">Login</el-button>
+          <el-button type="info" @click="resetLoginForm">Reset</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -31,9 +41,43 @@ export default {
     return {
       //   登陆表单的数据
       loginForm: {
-        username: 'admin',
-        password: 'admin-password'
+        username: '',
+        password: ''
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    // 重置登陆表单
+    resetLoginForm () {
+      //   console.log(this);
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      //   表单验证
+      //   this.$refs.loginFormRef.validate(valid => {
+      //     console.log(valid)
+      //   })
+
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // console.log(res)
+        if (res.meta.status !== 200) return console.log('Login failed!')
+        console.log('Login succeed!')
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
