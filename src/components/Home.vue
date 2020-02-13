51999cd5dcb1407bcab34b334f80db78d21d8e58
Jackyjk <2538808265@qq.com>
2020-02-13 17:52:45 +0800

    主页Header测试完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <el-button
-      type="info"
-      @click="logout"
-    >退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img class="img_logo" src="../assets/MrJ-logo.png" alt="">
+        <span style="color:red">XXX电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -19,4 +30,35 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-main {
+  background-color: rgb(161, 204, 206);
+}
+.el-aside {
+  background-color: rgb(24, 148, 148);
+}
+.home-container {
+  height: 100%;
+}
+.img_logo{
+  position: relative;
+  width: 100px;
+  height: 60px;
+
+}
 </style>
