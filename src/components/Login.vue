45d6252c7d0f1fac02061d86acd06a207233df05
Jackyjk <2538808265@qq.com>
2020-02-13 11:07:51 +0800

    数据绑定 完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!-- header -->
       <div class="avatar_box">
         <img src="../assets/logo.png" alt />
       </div>
+      <!-- login form -->
+      <el-form :model="loginForm" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">Login</el-button>
+          <el-button type="info">Reset</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      //   登陆表单的数据
+      loginForm: {
+        username: 'admin',
+        password: 'admin-password'
+      }
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -21,29 +48,43 @@ export default {}
 .login_box {
   width: 450px;
   height: 300px;
-  background-color: #ffffff;
+  background-color: #fff;
   border-radius: 3px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-}
-.avatar_box {
-  height: 130px;
-  width: 130px;
-  border: 1px solid #eee;
-  border-radius: 50%;
-  padding: 10px;
-  box-shadow: 0 0 10px #ddd;
-  position: absolute;
-  left:50%;
-  transform: translate(-50%,-50%);
-  background-color: #fff;
-  img {
-    width: 100%;
-    height: 100%;
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
     border-radius: 50%;
-    background-color: #eee;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
   }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入字体图标库
+import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 
