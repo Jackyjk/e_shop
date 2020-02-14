be5a01860c1da9bb955eef1dcefc62e27a2efb5a
Jackyjk <2538808265@qq.com>
2020-02-14 16:53:56 +0800

    完成用户搜索 表格渲染功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,26 +10,168 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <!-- 搜索与添加区域 -->
-      <el-row :gutter = "20">
+      <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
 
             <el-button
               slot="append"
               icon="el-icon-search"
+              @click="getUserList"
             ></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-            <el-button type="primary">添加用户</el-button>
+          <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+
+      <!-- 用户列表区域 -->
+      <el-table
+        :data="userlist"
+        border
+        stripe
+      >
+        <el-table-column type="index"></el-table-column>
+        <el-table-column
+          label="姓名"
+          prop="username"
+        ></el-table-column>
+        <el-table-column
+          label="邮箱"
+          prop="email"
+        ></el-table-column>
+        <el-table-column
+          label="电话"
+          prop="mobile"
+        ></el-table-column>
+        <el-table-column
+          label="角色"
+          prop="role_name"
+        ></el-table-column>
+        <el-table-column
+          label="状态"
+          prop="mg_state"
+        >
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="userStateChange(scope.row)"
+            ></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          width="180px"
+        >
+          <template slot-scope>
+            <!-- 修改按钮 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <!-- 删除按钮 -->
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
+            <!-- 分配角色权限按钮 -->
+            <el-tooltip
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button
+                type="warning"
+                icon="el-icon-setting"
+                size="mini"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1,2,5,10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      //   获取用户列表参数对象
+      queryInfo: {
+        query: '',
+        // 当前页数
+        pagenum: 1,
+        // 每页显示多少条数据
+        pagesize: 2
+      },
+      userlist: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取数据失败！！！')
+      }
+      this.userlist = res.data.users
+      this.total = res.data.total
+      console.log(res)
+    },
+    // 监听pagesize改变
+    handleSizeChange(newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听页码值改变
+    handleCurrentChange(newPage) {
+      // console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+
+    // 获取状态信息
+    async userStateChange(userInfo) {
+      // console.log(userInfo)
+      const { data: res } = await this.$http.put(
+        `users/${userInfo.id}/state/${userInfo.mg_state}`
+      )
+      if (res.meta.status !== 200) {
+        userInfo.mg_state = !userInfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      } else {
+        this.$message.success('更新用户状态成功')
+      }
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -18,7 +18,12 @@ import {
   BreadcrumbItem,
   Card,
   Row,
-  Col
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination
 } from 'element-ui'
 
 Vue.use(Button)
@@ -39,4 +44,9 @@ Vue.use(BreadcrumbItem)
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
 Vue.prototype.$message = Message
