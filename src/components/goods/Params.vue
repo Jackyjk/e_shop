e9f8c2bbc0f49c1ea690f2d86400d9ad92299e9e
Jackyjk <2538808265@qq.com>
2020-02-22 15:26:33 +0800

    分类参数->编辑/删除功能完成

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -35,15 +35,15 @@
             <el-table-column type="index"></el-table-column>
             <el-table-column label="参数名称" prop="attr_name"></el-table-column>
             <el-table-column label="操作">
-              <template slot-scope>
-                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.attr_id)">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeParams(scope.row.attr_id)">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
         </el-tab-pane>
         <!-- 添加静态属性 面板 -->
-        <el-tab-pane label="静态属性" name="only">
+        <el-tab-pane label=" 静态属性" name="only">
           <!-- 添加参数的按钮 -->
           <el-button type="primary" size="mini" :disabled="isBtnDisabled" @click="addDialogVisible=true">
             添加属性
@@ -56,9 +56,9 @@
             <el-table-column type="index"></el-table-column>
             <el-table-column label="属性名称" prop="attr_name"></el-table-column>
             <el-table-column label="操作">
-              <template slot-scope>
-                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.attr_id)">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeParams(scope.row.attr_id)">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -76,6 +76,34 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogaddDialogVisibleVisible = false">取 消</el-button>
         <el-button type="primary" @click="addParams">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 修改参数对话框 -->
+    <el-dialog :title="'修改'+titleText" :visible.sync="editDialogVisible" width="50%" @closed="editDialogClosed">
+      <!-- 修改参数 对话框 -->
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="editForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editParams">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 删除参数对话框 -->
+    <el-dialog :title="'删除'+titleText" :visible.sync="editDialogVisible" width="50%" @closed="editDialogClosed">
+      <!-- 修改参数 对话框 -->
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="editForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editParams">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -114,6 +142,20 @@ export default {
       },
       //   添加表单的验证规则对象
       addFormRules: {
+        attr_name: [
+          {
+            required: true,
+            message: '请输入参数名称',
+            trigger: 'blur'
+          }
+        ]
+      },
+      //   控制修改对话框 显示与隐藏
+      editDialogVisible: false,
+      //   修改的表单数据对象
+      editForm: {},
+      //   修改表单的验证规则对象
+      editFormRules: {
         attr_name: [
           {
             required: true,
@@ -195,6 +237,73 @@ export default {
         this.addDialogVisible = false
         this.getParamsData()
       })
+    },
+    // 点击按钮，展示修改对话框
+    async showEditDialog(attrId) {
+      // 查询当前参数的id
+      const { data: res } = await this.$http.get(
+        `categories/${this.cateId}/attributes/${attrId}`,
+        {
+          params: { attr_sel: this.activeName }
+        }
+      )
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数信息失败！！！')
+      }
+      this.editForm = res.data
+      this.editDialogVisible = true
+    },
+    // 重置修改的表单
+    editDialogClosed() {
+      this.$refs.editFormRef.resetFields()
+    },
+    // 点击按钮，修改参数信息
+    editParams() {
+      this.$refs.editFormRef.validate(async valid => {
+        if (!valid) return
+        const {
+          data: res
+        } = await this.$http.put(
+          `categories/${this.cateId}/attributes/${this.editForm.attr_id}`,
+          { attr_name: this.editForm.attr_name, attr_sel: this.activeName }
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('修改参数失败！！！')
+        }
+        this.$message.success('修改参数成功！！！')
+        this.getParamsData()
+        this.editDialogVisible = false
+      })
+    },
+    // 根据id 点击按钮，删除参数信息
+    async removeParams(attrId) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该参数, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+
+      // 用户取消删除操作
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除！')
+      }
+
+      //   删除的业务逻辑
+      const { data: res } = await this.$http.delete(
+        `categories/${this.cateId}/attributes/${attrId}`
+      )
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除参数信息失败！！')
+      }
+
+      this.$message.error('删除参数信息成功！！')
+      this.getParamsData()
     }
   },
   computed: {
