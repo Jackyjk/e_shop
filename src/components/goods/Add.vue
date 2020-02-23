9f8f9a452912a0d62bac1795be80cd5cd0d252d0
Jackyjk <2538808265@qq.com>
2020-02-23 12:51:03 +0800

    商品基本信息绘制  完成

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -22,10 +22,19 @@
 
       <!-- tabs栏区域-->
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
-        <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px;">
+        <el-tabs v-model="activeIndex" :tab-position="'left'">
           <el-tab-pane label="基本信息" name="0">
             <el-form-item label="商品名称" prop="goods_name">
               <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
             </el-form-item>
           </el-tab-pane>
           <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
@@ -46,12 +55,31 @@ export default {
       activeIndex: '0',
       //   添加商品的数据对象
       addForm: {
-        goods_name: ''
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0
       },
       addFormRules: {
         goods_name: {
           required: true,
-          message: ''
+          message: '请输入商品名称',
+          trigger: 'blur'
+        },
+        goods_price: {
+          required: true,
+          message: '请输入商品价格',
+          trigger: 'blur'
+        },
+        goods_weight: {
+          required: true,
+          message: '请输入商品重量',
+          trigger: 'blur'
+        },
+        goods_number: {
+          required: true,
+          message: '请输入商品数量',
+          trigger: 'blur'
         }
       }
     }
