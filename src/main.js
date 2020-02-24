bfe2c811648b79e5565727e8aeaf6e6544fbf66c
Jackyjk <2538808265@qq.com>
2020-02-24 14:33:18 +0800

    增加进度条loading效果

--- src/main.js
+++ src/main.js
@@ -15,12 +15,24 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
+// 导入NProgress包 JS以及 样式表
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request 拦截器中 展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
-  console.log(config)
+  // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须添加下面这一行
+  return config
+})
+// 在response拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
