14eadfbb86d1bfe8772d4ffca8e2c8291ac2dd4e
Jackyjk <2538808265@qq.com>
2020-02-24 11:43:26 +0800

    订单管理大致框架完成，地址填写模板完成

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
 import List from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
+import Order from './components/order/Order.vue'
 
 Vue.use(Router)
 
@@ -55,6 +56,10 @@ const router = new Router({
     {
       path: '/goods/add',
       component: Add
+    },
+    {
+      path: '/orders',
+      component: Order
     }
     ]
   }]
